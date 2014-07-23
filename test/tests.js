3f628f0ea1c2842229a29909393e3e163a2f7cb9
panying <anylain@gmail.com>
2014-07-24 02:48:45 +0800

    * 猛烈的修改了代码风格

--- test/tests.js
+++ test/tests.js
@@ -1,236 +1,241 @@
-QUnit.test("Get Version Test", function( assert ) {
+QUnit.test("Get Version Test", function (assert) {
     var rc = new $.RestClient();
     assert.ok(rc.getVersion(), "version = " + rc.getVersion());
 });
 
-QUnit.test("Default Instance Test", function( assert ) {
+QUnit.test("Default Instance Test", function (assert) {
     assert.ok($.rest, "Has default instance with '$.rest'");
     assert.deepEqual($.rest.getOptions(), $.RestClient.options.defaults, "Init default options on create default instance");
 });
 
-QUnit.test("URL Build Test", function( assert ) {
+QUnit.test("URL Build Test", function (assert) {
     var rc = new $.RestClient();
+    var url, good;
+    url = rc.buildUrl('users/{id}', {id: 123});
+    good = 'users/123';
+    assert.equal(url, good, "Simple url params");
 
-    var url = rc.buildUrl('users/{id}', {id:123});
-    var good = 'users/123';
-    assert.equal( url, good, "Simple url params" );
-    
-    var url = rc.buildUrl('users/{id}', null, {id:123});
-    var good = 'users/123';
-    assert.equal( url, good, "Simple path params" );
-    
-    var url = rc.buildUrl('users', null, null, {id:123});
-    var good = 'users?id=123';
-    assert.equal( url, good, "Simple query params" );
-    
-    var url = rc.buildUrl('users/{id}', {id:456}, {id:123});
-    var good = 'users/123';
-    assert.equal( url, good, "Path params priority than the url params" );
-    
-    var url = rc.buildUrl('users', {id:456}, null, {id:123});
-    var good = 'users?id=123';
-    assert.equal( url, good, "Query params priority than the url params" );
-    
-    var url = rc.buildUrl('users/{id}/book/{book}', {id:123, key:"python", key1:"lulu"}, {book:"rest_client"}, {key:"javascript"}, '.pdf');
-    var good = 'users/123/book/rest_client.pdf?key=javascript&key1=lulu';
-    assert.equal( url, good, "Combo (Has ext)" );
-    
-    try{
-    	rc.buildUrl('users/{id}', null);
-    	assert.ok(false, "Throw an exception on can't found params.")
-    }catch (e) {
-    	assert.ok(true, "Throw an exception on can't found params.")
-	}
-    
-    var url = rc.buildUrl('users/{id}/books/{book}', {id:123, book:'Spring实战(第三版)', page:12, keyword:'http://spring.io/'}, null, null, '.json');
-    var good = 'users/123/books/Spring%E5%AE%9E%E6%88%98(%E7%AC%AC%E4%B8%89%E7%89%88).json?page=12&keyword=http%3A%2F%2Fspring.io%2F';
-    assert.equal( url, good, "Param encoding" );
+    url = rc.buildUrl('users/{id}', null, {id: 123});
+    good = 'users/123';
+    assert.equal(url, good, "Simple path params");
+
+    url = rc.buildUrl('users', null, null, {id: 123});
+    good = 'users?id=123';
+    assert.equal(url, good, "Simple query params");
+
+    url = rc.buildUrl('users/{id}', {id: 456}, {id: 123});
+    good = 'users/123';
+    assert.equal(url, good, "Path params priority than the url params");
+
+    url = rc.buildUrl('users', {id: 456}, null, {id: 123});
+    good = 'users?id=123';
+    assert.equal(url, good, "Query params priority than the url params");
+
+    url = rc.buildUrl('users/{id}/book/{book}', {id: 123, key: "python", key1: "lulu"}, {book: "rest_client"}, {key: "javascript"}, '.pdf');
+    good = 'users/123/book/rest_client.pdf?key=javascript&key1=lulu';
+    assert.equal(url, good, "Combo (Has ext)");
+
+    try {
+        rc.buildUrl('users/{id}', null);
+        assert.ok(false, "Throw an exception on can't found params.");
+    } catch (e) {
+        assert.ok(true, "Throw an exception on can't found params.");
+    }
+
+    url = rc.buildUrl('users/{id}/books/{book}', {id: 123, book: 'Spring实战(第三版)', page: 12, keyword: 'http://spring.io/'}, null, null, '.json');
+    good = 'users/123/books/Spring%E5%AE%9E%E6%88%98(%E7%AC%AC%E4%B8%89%E7%89%88).json?page=12&keyword=http%3A%2F%2Fspring.io%2F';
+    assert.equal(url, good, "Param encoding");
 });
 
-QUnit.test("Init Options Test", function( assert ) {
+QUnit.test("Init Options Test", function (assert) {
     var rc1 = new $.RestClient();
     var rc2 = new $.RestClient($.RestClient.options.json);
     var rc3 = new $.RestClient($.RestClient.options.xml);
     assert.deepEqual(rc1.getOptions(), $.RestClient.options.defaults, "Init default options on create new instance by no args");
     assert.deepEqual(rc2.getOptions(), $.RestClient.options.json, "Init options on create new instance by $.RestClient.options.json");
     assert.deepEqual(rc3.getOptions(), $.RestClient.options.xml, "Init options on create new instance by $.RestClient.options.xml");
-    
-    var customOption = {dataType : "text", headers : { accept : "text/plain" }};
+
+    var customOption = {dataType: "text", headers: { accept: "text/plain" }};
     var rc4 = new $.RestClient(customOption);
     assert.deepEqual(rc4.getOptions(), customOption, "Init options on create new instance by custom options");
 });
 
-QUnit.test("Request Build Test", function( assert ) {
-	
-    var methods = ['GET','POST','PUT','DELETE'];
-    $(methods).each(function(index, method){
+QUnit.test("Request Build Test", function (assert) {
 
-    	var rc = new $.RestClient().updateOptions({
-    		error: function(message, jqXHR, textStatus, errorThrown) {
-    			if(errorThrown) throw new Error(errorThrown);
-    			else throw new Error(message);
-    		}
-    	});
-    	
-    	var serializeRequestBodyCalled = false;
-    	var compatibleCalled = false;
-    	var request = rc.buildRequest(method, {
-    		serializeRequestBody: function(){
-    			serializeRequestBodyCalled = true;
-    		},
-    		compatible: function(mth, req) {
-    			compatibleCalled = true;
-    		}
-    	});
-    	assert.ok(serializeRequestBodyCalled, "(Method=" + method + ") Auto serialize request body");
-    	assert.ok(compatibleCalled, "(Method=" + method + ") Call compatible function");
+    var methods = ['GET', 'POST', 'PUT', 'DELETE'];
+    $(methods).each(function (index, method) {
 
-		assert.ok(request.success, "(Method=" + method + ") Request has success callback function");
-		assert.ok(request.error, "(Method=" + method + ") Request has error callback function");
+        var rc = new $.RestClient().updateOptions({
+            error: function (message, jqXHR, textStatus, errorThrown) {
+                if (errorThrown) {
+                    throw new Error(errorThrown);
+                }
+                else {
+                    throw new Error(message);
+                }
+            }
+        });
+
+        var serializeRequestBodyCalled = false;
+        var compatibleCalled = false;
+        var request = rc.buildRequest(method, {
+            serializeRequestBody: function () {
+                serializeRequestBodyCalled = true;
+            },
+            compatible: function () {
+                compatibleCalled = true;
+            }
+        });
+        assert.ok(serializeRequestBodyCalled, "(Method=" + method + ") Auto serialize request body");
+        assert.ok(compatibleCalled, "(Method=" + method + ") Call compatible function");
+
+        assert.ok(request.success, "(Method=" + method + ") Request has success callback function");
+        assert.ok(request.error, "(Method=" + method + ") Request has error callback function");
     });
 });
 
-QUnit.test("Update Options Test", function( assert ) {
+QUnit.test("Update Options Test", function (assert) {
     var rc = new $.RestClient();
     var source = {
-    	"number" : 123.321,
-    	"str" : "some string",
-		"array" : [ 1, 2, 3, 4 ],
-		"obj" : {
-			a : "apple",
-			b : "banana"
-		}
-	};
-    
+        "number": 123.321,
+        "str": "some string",
+        "array": [ 1, 2, 3, 4 ],
+        "obj": {
+            a: "apple",
+            b: "banana"
+        }
+    };
+
     var newValue = {
-    	"str" : "new string",
-    	"array" : [ 'alpha', 'beta' ],
-    	"obj" : {
-    		b : "blue",
-    		c : "cat"
-    	}
+        "str": "new string",
+        "array": [ 'alpha', 'beta' ],
+        "obj": {
+            b: "blue",
+            c: "cat"
+        }
     };
-    
+
     var forceResult = {
-    	"number" : 123.321,
-    	"str" : "new string",
-    	"array" : [ 'alpha', 'beta' ],
-		"obj" : {
-			a : "apple",
-    		b : "blue",
-    		c : "cat"
-		}
+        "number": 123.321,
+        "str": "new string",
+        "array": [ 'alpha', 'beta' ],
+        "obj": {
+            a: "apple",
+            b: "blue",
+            c: "cat"
+        }
     };
-    
+
     var noForceResult = {
-    	"number" : 123.321,
-    	"str" : "some string",
-		"array" : [ 1, 2, 3, 4 ],
-		"obj" : {
-			a : "apple",
-			b : "banana",
-	    	c : "cat"
-		}
+        "number": 123.321,
+        "str": "some string",
+        "array": [ 1, 2, 3, 4 ],
+        "obj": {
+            a: "apple",
+            b: "banana",
+            c: "cat"
+        }
     };
-    
+
     assert.notEqual(rc._updateOptions(source, {}, true), source, "Create a new object for result");
     assert.deepEqual(rc._updateOptions(source, newValue, true), forceResult, "Update options by force (use new value with conflict)");
     assert.deepEqual(rc._updateOptions(source, newValue, false), noForceResult, "Update options by no force (use source value with conflict)");
 });
 
-QUnit.test("Compatible Test", function( assert ) {
+QUnit.test("Compatible Test", function (assert) {
     var rc = new $.RestClient();
     var request;
-    
-    var methods = ['GET','POST','PUT','DELETE'];
-    $(methods).each(function(index, method){
+
+    var methods = ['GET', 'POST', 'PUT', 'DELETE'];
+    $(methods).each(function (index, method) {
         request = {
-            	compatible : 'x-method'
-            };
-            rc._compatibleHandler(method, request);
-            assert.equal(request.type, 'POST', "(Method=" + method + ") In 'x-method' model, request method will be replaced by 'POST'");
-            assert.equal(request.headers['x-method-override'], method, "(Method=" + method + ") In 'x-method' model, the real method will write in http header 'x-method-override'");
-            
-            request = {
-            	compatible : 'url'
-            };
-            rc._compatibleHandler(method, request);
-            assert.equal(request.type, 'POST', "(Method=" + method + ") In 'url' model, request method will be replaced by 'POST'");
-            assert.equal(request.urlParams['__method'], method, "(Method=" + method + ") In 'url' model, the real method will write in url query parameter '__method'");
-            
-            var called = false;
-            request = {
-            	compatible : function(mth, req){
-            		if(mth == method && request===req)
-            			called=true;
-            	}
-            };
-            rc._compatibleHandler(method, request);
-            assert.ok(called, "(Method=" + method + ") When compatible is function, they will be call to build request");
+            compatible: 'x-method'
+        };
+        rc._compatibleHandler(method, request);
+        assert.equal(request.type, 'POST', "(Method=" + method + ") In 'x-method' model, request method will be replaced by 'POST'");
+        assert.equal(request.headers['x-method-override'], method, "(Method=" + method + ") In 'x-method' model, the real method will write in http header 'x-method-override'");
+
+        request = {
+            compatible: 'url'
+        };
+        rc._compatibleHandler(method, request);
+        assert.equal(request.type, 'POST', "(Method=" + method + ") In 'url' model, request method will be replaced by 'POST'");
+        assert.equal(request.urlParams.__method, method, "(Method=" + method + ") In 'url' model, the real method will write in url query parameter '__method'");
+
+        var called = false;
+        request = {
+            compatible: function (mth, req) {
+                if (mth === method && request === req) {
+                    called = true;
+                }
+            }
+        };
+        rc._compatibleHandler(method, request);
+        assert.ok(called, "(Method=" + method + ") When compatible is function, they will be call to build request");
     });
 });
 
-QUnit.test("Send Request Test", function( assert ) {
-	var optionsErrorHandlerCalled = false;
-	var optionsCompleteHandlerCalled = false;
-	var requestErrorHandlerCalled = false;
-	var requestCompleteHandlerCalled = false;
-	var addonErrorHandlerCalled = false;
-	var addonCompleteHandlerCalled = false;
-	
-	function resetMarks() {
-		optionsErrorHandlerCalled = false;
-		optionsCompleteHandlerCalled = false;
-		requestErrorHandlerCalled = false;
-		requestCompleteHandlerCalled = false;
-		addonErrorHandlerCalled = false;
-		addonCompleteHandlerCalled = false;
-	}
-	
-	var rc = new $.RestClient().updateOptions({
-		error: function(message, jqXHR, textStatus, errorThrown){
-			optionsErrorHandlerCalled = true;
-		},
-		complete: function() {
-			optionsCompleteHandlerCalled = true;
-		}
-	});
-	var badRequest = {url:"{noexist}"};
-	
-	rc.sendRequest("GET", badRequest);
-	
-	assert.ok(optionsErrorHandlerCalled, "Call options error handler when request build fault.");
-	assert.ok(optionsCompleteHandlerCalled, "Call options complete handler when request build fault.");
+QUnit.test("Send Request Test", function (assert) {
+    var optionsErrorHandlerCalled = false;
+    var optionsCompleteHandlerCalled = false;
+    var requestErrorHandlerCalled = false;
+    var requestCompleteHandlerCalled = false;
+    var addonErrorHandlerCalled = false;
+    var addonCompleteHandlerCalled = false;
 
-	resetMarks()
-	
-	badRequest = {url:"{noexist}", 
-		error: function(message, jqXHR, textStatus, errorThrown){
-			requestErrorHandlerCalled = true;
-		},
-		complete: function() {
-			requestCompleteHandlerCalled = true;
-		}
-	};
-	
-	rc.sendRequest("GET", badRequest);
-	
-	assert.ok(!optionsErrorHandlerCalled && requestErrorHandlerCalled, "Call request error handler priority when request build fault.");
-	assert.ok(!optionsCompleteHandlerCalled && requestCompleteHandlerCalled, "Call request complete handler priority when request build fault.");
-	
-	resetMarks()
-	
-	rc.sendRequest("GET", badRequest, {
-		error: function(message, jqXHR, textStatus, errorThrown){
-			addonErrorHandlerCalled = true;
-		},
-		complete: function() {
-			addonCompleteHandlerCalled = true;
-		}
-	});
-	
-	assert.ok(!optionsErrorHandlerCalled && !requestErrorHandlerCalled && addonErrorHandlerCalled, "Call addon error handler priority when request build fault.");
-	assert.ok(!optionsCompleteHandlerCalled && !requestCompleteHandlerCalled && addonCompleteHandlerCalled, "Call addon complete handler priority when request build fault.");
-	
-	
+    function resetMarks() {
+        optionsErrorHandlerCalled = false;
+        optionsCompleteHandlerCalled = false;
+        requestErrorHandlerCalled = false;
+        requestCompleteHandlerCalled = false;
+        addonErrorHandlerCalled = false;
+        addonCompleteHandlerCalled = false;
+    }
+
+    var rc = new $.RestClient().updateOptions({
+        error: function () {
+            optionsErrorHandlerCalled = true;
+        },
+        complete: function () {
+            optionsCompleteHandlerCalled = true;
+        }
+    });
+    var badRequest = {url: "{noexist}"};
+
+    rc.sendRequest("GET", badRequest);
+
+    assert.ok(optionsErrorHandlerCalled, "Call options error handler when request build fault.");
+    assert.ok(optionsCompleteHandlerCalled, "Call options complete handler when request build fault.");
+
+    resetMarks();
+
+    badRequest = {url: "{noexist}",
+        error: function () {
+            requestErrorHandlerCalled = true;
+        },
+        complete: function () {
+            requestCompleteHandlerCalled = true;
+        }
+    };
+
+    rc.sendRequest("GET", badRequest);
+
+    assert.ok(!optionsErrorHandlerCalled && requestErrorHandlerCalled, "Call request error handler priority when request build fault.");
+    assert.ok(!optionsCompleteHandlerCalled && requestCompleteHandlerCalled, "Call request complete handler priority when request build fault.");
+
+    resetMarks();
+
+    rc.sendRequest("GET", badRequest, {
+        error: function () {
+            addonErrorHandlerCalled = true;
+        },
+        complete: function () {
+            addonCompleteHandlerCalled = true;
+        }
+    });
+
+    assert.ok(!optionsErrorHandlerCalled && !requestErrorHandlerCalled && addonErrorHandlerCalled, "Call addon error handler priority when request build fault.");
+    assert.ok(!optionsCompleteHandlerCalled && !requestCompleteHandlerCalled && addonCompleteHandlerCalled, "Call addon complete handler priority when request build fault.");
+
+
 });
